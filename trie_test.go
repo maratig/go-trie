bbdec5b1b1972f94e35eb0d8594dc43ceb3da423
Marat Gafiyatullin <gafik07@mail.ru>
2019-10-16 23:44:01 +0300

    Some changes in test

--- trie_test.go
+++ trie_test.go
@@ -13,28 +13,28 @@ func TestTrie(t *testing.T) {
 	tr := Trie{}
 	// Generate pseudo words
 	word := make([]rune, 8, 8)
-	for i := 0; i < 1000000; i++ {
+	for i := 0; i < 100000; i++ {
 		for j := 0; j < 8; j++ {
 			word[j] = rand.Int31n(25) + 97
 		}
-		tr.Set(word)
+		tr.Set(string(word), true)
 	}
-	tr.Set([]rune("mother"))
-	tr.Set([]rune("father"))
+	tr.Set("mother", true)
+	tr.Set("father", true)
 
-	hasMother := tr.Has([]rune("mother"))
-	hasFather := tr.Has([]rune("father"))
-	hasSister := tr.Has([]rune("sister"))
+	hasMother, _ := tr.Get("mother")
+	hasFather, _ := tr.Get("father")
+	hasSister, _ := tr.Get("sister")
 
-	if !hasMother {
+	if hasMother == nil {
 		t.Fatalf(`error while searching for "mother" word`)
 	}
 
-	if !hasFather {
+	if hasFather == nil {
 		t.Fatalf(`error while searching for "father" word`)
 	}
 
-	if hasSister {
+	if hasSister == nil {
 		t.Fatalf(`error while searching for "sister" word`)
 	}
 }
